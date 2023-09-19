21d1b8c665f2fc5e178d6c1872aea9b09c775590
Raymond Lowe <raymondclowe@gmail.com>
2023-09-19 17:37:19 +0800

    fix get_id concaten

--- utils.py
+++ utils.py
@@ -3,5 +3,6 @@
 import hashlib
 
 def get_id(chatID, obj):
-  id = hashlib.md5(str(chatID)+str(obj).encode('utf-8')).hexdigest()
+  # id = hashlib.md5(str(chatID)+str(obj).encode('utf-8')).hexdigest()
+  id = hashlib.md5((str(chatID) + str(obj)).encode('utf-8')).hexdigest()
   return id
